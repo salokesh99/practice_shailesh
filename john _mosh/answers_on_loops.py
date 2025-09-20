ba310302f9a1e4f850b52e2816ce49223d950d87
salokesh99 <salokesh99@gmail.com>
2025-09-20 20:20:57 +0530

    new files

--- john _mosh/answers_on_loops.py
+++ john _mosh/answers_on_loops.py
@@ -96,13 +96,251 @@
 
 
 #9-----Write a program to calculate the factorial of a number using a loop.
-number=6
+# number=6
+# i=1
+# fact=1
+# while i<=number:
+#     fact*=i
+#     i+=1
+# print(fact)  
 
-fact=1
-for i in range(1,number+1) :
-    fact*=i
-    
-print(fact)  
+
+
+
+
+#10-----Write a program to print all even numbers between 1 and 50.
+# i=1
+# while i<=50:
+#     if i%2==0:
+#         print(i)
+        
+#     i+=1  
+
+
+
+
+#11----Write a program to reverse a given number (e.g., 123 → 321).
+# number=123
+# temp=0
+# while number>0:
+#     temp=temp*10+number%10
+#     number=number//10
+# rev=temp
+# print(rev)
+
+
+
+#12----Write a program to check whether a number is a palindrome (e.g., 121 → palindrome).
+# number=int(input("enter number : "))
+# original_number=number
+# temp=0
+# while number>0:
+#     temp=temp*10+number%10
+#     number=number//10
+# rev=temp
+# if rev==original_number:
+#     print("it is a palondrome")
+# else:
+#     print("it is not a palindrome")    
+
+
+
+
+#13----Write a program to find the sum of digits of a number (e.g., 123 → 6).
+# number=125
+# temp1=0
+# while number>0:
+#     temp=number%10
+#     temp1=temp1+temp
+#     number=number//10
+# print(temp1)    
+
+
+
+
+
+#14-----Write a program to print the Fibonacci series up to 10 terms.
+#(sum of previous number)
+# number=10
+# a=1
+# b=0
+# i=0
+# temp=0
+# while i<number:
+#     print(b)
+#     temp=a
+#     a=a+b
+#     b=temp
+#     i+=1
+
+
+
+
+#15----Write a program to check whether a number is positive or negative.
+# a=10
+# if type(a)==int:   #to check number is int type or not
+#     if a>0:
+#         print("positive")
+#     elif a<0:
+#         print("negative")
+#     else:
+#         print("zero")
+
+
+
+#16-----write a program to find the largest of three numbers using nested if.
+# a=10
+# b=5
+# c=8
+# if a>=b:
+#     if a>=c:
+#         print("a is largest")
+#     else:
+#         print("c is laegest")
+# else:
+#     if b>=a:
+#         print("b is largest")
+#     else:
+#         primt("c is largest")
+
+
+
+
+#17-----Write a program to check whether a given number is divisible by 5 and 11 or not.
+# a=10
+# if a%5==0 and a%11==0:
+#     print("number is divisible by both")
+# else:
+#     print("it is not divisble by both")
+
+
+
+#18-----Write a program to check whether a year is a leap year or not.
+# number=int(input("enter numebr :"))
+# if number%4==0:
+#     print("it is a leap year")
+# else:
+#     print("not a leap year")
+
+
+
+
+# 19-----Write a program to accept a character from the user and check whether it is a vowel or consonant.
+# ch=input("enter character")
+# if ch.lower() in ('a','e','i','o','u'):
+#     print("it is a vowel")
+# else:
+#     print("it is consonent")
+
+
+
+#20-----Write a program to check whether a person is eligible to vote (age ≥ 18).
+# age=25
+# if age>=18:
+#     print("person is eligible")
+# else:
+#     print("not eligible")
+
+
+
+
+#21-----Write a program to accept marks from a student and print their grade:
+# 90–100 → A
+# 75–89 → B
+# 50–74 → C
+# Below 50 → Fail.
+
+# marks=int(input("marks : "))
+# if 90<=marks <=100:
+#     print("grade=A")
+# elif 75 <=marks<=89:
+#     print("grade=B")
+# elif 50<=marks <=74:
+#     print("grade=C")
+# elif marks<50:
+#     print("fail")
+# else:
+#     print("invalid")
+
+
+
+
+#22----Write a program to check whether a given number is a single-digit, double-digit, or more than two digits.
+# number=1234
+# if -9<=number<=9:
+#     print("it is single digit")
+# elif -99<=number<=99:
+#     print("it is double digit")
+# else:
+#     print("more than double digit")
+
+
+
+
+#23----Write a program to accept three angles of a triangle and check whether the triangle is valid or not (sum = 180°).
+# a=int(input("first angle: "))
+# b=int(input("second angle : "))
+# c=int(input("third angle : "))   
+
+# if a>0 and b>0 and c>0 and (a+b+c)==180:
+#     print("triangle is valid")
+# else:
+#     print("triangle is invalid")   
+
+
+
+#24----Write a program to check whether a character entered by the user is an uppercase letter, lowercase letter, digit, or special symbol.
+# ch=input("enter a character : ")
+# if len(ch) !=1:
+#     print("enter a only one cha")
+# elif 'A'<=ch<='Z':
+#     print("it is uppercase")
+# elif 'a'<=ch<='z':
+#     print("lowercase")
+# elif '0'<=ch<='9':
+#     print("digit")
+# else:
+#     print("special symbol")
+
+
+
+
+#25----Write a program to accept the age of a person and classify them as:
+
+# Child (0–12)
+
+# Teenager (13–19)
+
+# Adult (20–59)
+
+# Senior Citizen (60 and above).
+
+# age=int(input("age of person : "))
+# if 0<=age<=12:
+#     print("child")
+# elif 13<=age<=19:
+#     print("teenage")
+# elif 20<=age<=59:
+#     print("adult")
+# else:
+#     print("senior")
+
+
+
+def call_thisfunction(n):
+    print("this function is called")
+    if n==1:
+      print("entered if","value of n is",n)
+      
+      return 1
+    else:
+       print("entered else"," value of n is ",n)
+       return "not 1"
+
+x=call_thisfunction(5)
+print("value of x",x)
+
+  
 
 
 
---
+++ john _mosh/answers_on_strings.py
@@ -0,0 +1,173 @@
+#1-----Write a program to accept a string from the user and print it.
+# name=input("enter name : ")
+# print(name)
+
+
+
+#2-----Write a program to find the length of a string without using len().
+# name="shailesh"
+# count=0
+# for ch in (name):
+#     count+=1
+# print(count)
+
+
+
+#3-----Write a program to convert a string to uppercase.
+# name="shailesh ajja"
+# print(name.upper())
+
+
+
+#4-----Write a program to convert a string to lowercase.
+# name="SHAILesh ajJA"
+# print(name.lower())
+
+
+
+#5----Write a program to count how many vowels are in a string.
+# name="shailesh ajja"
+# vowels="aeiouAEIOU"
+# count=0
+# for ch in (name):
+#    if ch in vowels:
+#       count+=1
+# print(count)
+
+
+
+
+#6-----Write a program to reverse a string.
+# name="shailesh"
+# reverse="" 
+# for char in name:
+#     reverse=char+reverse         # without loop use this reverse=name[::-1]
+# print(reverse)
+
+
+
+
+#7-----Write a program to check whether a string is a palindrome (same forward and backward).
+# name="rar"
+# reverse=name[::-1]
+# if name==reverse:
+#     print("it is palindrome")
+# else:
+#     print("it is not palindrome")
+
+
+
+
+
+#8-----Write a program to count the number of words in a string.
+# s=input(" enter words : ")
+# count=1
+
+# while s[0]==" ":
+#     s=s[1:]
+# print(s)
+# while s[-1]==" ":
+    
+#     s=s[:-2]
+# print(s)
+# for word in s:
+#     if word==" ":
+#         count+=1
+# print(count)
+
+
+
+
+# another solution
+# a="    shailesh ajja "
+# count=1
+# a=a.strip(" ")
+# #print(a.split())
+# for word in a:
+    
+#     if word==" ":
+#         count+=1
+# print(count)
+
+
+
+
+#9------Write a program to replace all spaces in a string with hyphens (-).
+# a="   raj   "
+# print(a.replace(" ","_"))
+
+
+
+#10-----Write a program to check whether a given substring is present in the string or not.
+# a="shailesh"
+# b="ai"
+# if b in a:
+#     print("is present")
+# else:
+#     print("not present")
+
+
+
+#11-----Write a program to remove all vowels from a string.
+# a="shailesh"
+# b="aeiouAEIOU"
+# res=""
+# for char in a:
+#     if char  not in b:
+#         res=res+char
+# print(res)
+
+
+
+
+#12----Write a program to find the first and last character of a string.
+# a="shailesh"
+# print(a[0],a[-1])
+
+
+
+#13----Write a program to count the frequency of each character in a string.
+# a="shailesh"
+# b={}
+
+# for ch in a:
+#     if ch not in b:
+#         b[ch]=1
+#         print(b[ch])
+        
+#     else:
+#         b[ch]+=1
+    
+# for ch in b:
+#     print(b[ch])    
+
+
+
+
+#14-----Write a program to check whether two strings are equal or not.
+# a="shailesh"
+# b="ajja"
+# c=len(a)
+# d=len(b)
+# if c==d:
+#     print("both are equal")
+# else :
+#     print("both are not equals")
+
+
+
+
+#15----Write a program to accept a string and print only the digits present in it.
+# a=input("enter : ")
+
+# for ch in a:
+#     if '-9'<=ch<='9':
+
+#         print(ch)
+
+
+
+
+
+        
+
